6a8e73ec7aac5de7218613d4b3f45281f0f01b19
Brian Vaughn <bvaughn@fb.com>
2017-02-18 12:44:07 -0800

    Tweaked demo site slide

--- source/website/Slides/3.js
+++ source/website/Slides/3.js
@@ -4,16 +4,16 @@ import { ContentSlide, Step } from '../../modules'
 const slide = ({ isPresenterMode }) => (
   <ContentSlide>
     <h1>{slide.title}</h1>
-    {isPresenterMode && (
-      <em>
-        <i className='fa fa-user-secret' /> This sentence (and the grayed-out bullet points below) are only visible to the presenter.
-      </em>
-    )}
     <ul>
       <li>Slides can contain presenter notes.</li>
       <Step index={1}><li>Type "p" to open a synced presenter window</li></Step>
       <Step index={2}><li>That window and this will be synced to stay on the same page</li></Step>
     </ul>
+    {isPresenterMode && (
+      <em>
+        <i className='fa fa-user-secret' /> It will also show upcoming slide steps and presenter-only notes (like this one).
+      </em>
+    )}
   </ContentSlide>
 )
 
